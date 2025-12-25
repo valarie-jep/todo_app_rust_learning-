a3f796f9399a987244d75bb5beededcb44612716
Valarie Jepleting <valariejepleting@gmail.com>
2025-12-25 21:30:50 +0300

    Read user menu choice from terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to your Rust To-Do List!");
+    println!("1. Add a Task");
+    println!("2. View Tasks");
+    println!("3. Mark Task as Completed");
+    println!("4. Delete Task");
+    println!("5. Exit");
+
+    println!("\nEnter your choice:");
+
+    let mut choice = String::new();
+
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read input");
+
+    println!("You chose: {}", choice.trim());
 }
+
